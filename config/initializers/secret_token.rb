767594a570dd02423710b7fc9b86e206915a6df2
Tony Guntharp <fusion94@damagestudios.net>
2013-02-23 09:58:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scotchbible::Application.config.secret_token = '576ecf792bd4fab565c944ec0a436ac923396690a07296bb1869856fbebde677f7fb783f3666810325eec84c3780fa4e2c28c20772ac81464fdac072cd92aa88'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Scotchbible::Application.routes.draw do
+ 
+  #...
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => "home#index"
+end
